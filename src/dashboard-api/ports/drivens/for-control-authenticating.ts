73928f35615015cbeff478c2b71ab3aea0601b5a
JosseGonnza <accion.quevedo@hotmail.com>
2024-09-29 17:00:58 +0100

    ✨ feat: create interface for Permissions

--- src/dashboard-api/ports/drivens/for-control-authenticating.ts
+++ src/dashboard-api/ports/drivens/for-control-authenticating.ts
@@ -3,6 +3,11 @@ export interface AuthDetails {
     refreshToken: string;
 }
 
+export interface Permissions {
+    admin: boolean;
+    user: boolean;
+}
+
 export interface ForControlAuthenticating {
     getAuthDetails(email: string, password: string): Promise<AuthDetails>;
     getPermissions(email: string, password: string): Promise<Permissions>;
