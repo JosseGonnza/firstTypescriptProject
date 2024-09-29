ffa106c652e65ef180810c3b85610f57b32836d1
JosseGonnza <accion.quevedo@hotmail.com>
2024-09-29 17:42:13 +0100

    ✨ feat: create for-repo-querying.ts

--- src/dashboard-api/ports/drivens/for-repo-querying.ts
+++ src/dashboard-api/ports/drivens/for-repo-querying.ts
@@ -1,6 +1,9 @@
-import {User} from "../../../repository/app/schemas";
+import {User as RepoUser} from "../../../repository/app/schemas";
+import {User} from "../drivers";
 
+//TODO: Utilizamos el User de schemas como RepoUser para devolver valores
+// Utilizamos el User de for-authenticating para insertar
 export interface ForRepoQuerying {
-    getUser(email: string): Promise<User>;
-    createUser(user: User, password: string): Promise<User>;
+    getUser(email: string): Promise<RepoUser>;
+    createUser(user: User, password: string): Promise<RepoUser>;
 }
