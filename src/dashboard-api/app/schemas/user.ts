fad0ecf91ec5723b71d076bbab319aca25b78f12
JosseGonnza <accion.quevedo@hotmail.com>
2024-09-29 20:11:31 +0100

    ✨ feat: control within ‘schemas’ all the Types of our api

---
+++ src/dashboard-api/app/schemas/user.ts
@@ -0,0 +1,13 @@
+export interface AuthenticatedUser {
+    id: string;
+    email: string;
+    name: string;
+    token: string;
+    refreshToken: string;
+}
+
+//TODO: Con Omit dejas de utilizar en el nuevo type las propiedades que le pasas
+// export type User = Omit<AuthenticatedUser, 'id'| 'token' | 'refreshToken'>;
+
+//TODO: Con Pick utilizas sólo las que le pasas
+export type User = Pick<AuthenticatedUser, 'email' | 'name'>;
