1cc4a983b28eb0ba28e8a91b953728fd70e7a305
JosseGonnza <accion.quevedo@hotmail.com>
2024-09-29 18:32:56 +0100

    ✨ feat: create repo-querier-stub.ts

---
+++ src/dashboard-api/adapters/drivens/repo-querier-stub.ts
@@ -0,0 +1,18 @@
+import {ForRepoQuerying} from "../../ports/drivens";
+import { User as RepoUser } from "../../../repository/app/schemas";
+import {User} from "../../ports/drivers";
+
+const userMock: RepoUser = {
+    id: '1',
+    name: 'Yasmi',
+    email: 'yasmi@test.com'
+}
+
+export class RepoQuerierStub implements ForRepoQuerying {
+    getUser(email: string): Promise<RepoUser> {
+        return Promise.resolve(userMock);
+    }
+    createUser(user: User, password: string): Promise<RepoUser> {
+        return Promise.resolve(userMock);
+    }
+}
