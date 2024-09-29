697ff8985662e7f100152f039ea43fe146a52f23
JosseGonnza <accion.quevedo@hotmail.com>
2024-09-29 16:39:03 +0100

    ✨ feat: create for-control-authenticating.ts

---
+++ src/dashboard-api/ports/drivens/for-control-authenticating.ts
@@ -0,0 +1,9 @@
+export interface AuthDetails {
+    token: string;
+    refreshToken: string;
+}
+
+export interface ForControlAuthenticating {
+    getAuthDetails: (email: string, password: string) => Promise<AuthDetails>;
+    getPermissions: (email: string, password: string) => Promise<Permissions>;
+}
